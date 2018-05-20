668292b41d3b071d7414dc35be2835fbc42a5714
Clayder <peterclayder@gmail.com>
2018-05-20 08:53:28 -0300

    [13] Criei a classe AulaAluno.

---
+++ gerenciador-curso-java/src/main/java/com/gerenciador/Model/AulaAluno/AulaAluno.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gerenciador.Model.AulaAluno;
+
+import java.sql.SQLException;
+
+public class AulaAluno extends AulaAlunoDAO implements IAulaAluno {
+    
+    public AulaAluno(Integer fkAluno, Integer fkAula) throws SQLException {
+        super(fkAluno, fkAula);
+    }
+
+}

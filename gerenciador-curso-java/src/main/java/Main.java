6c549dd9fc9c66e784be85d197213ca41cb0e87c
Clayder <peterclayder@gmail.com>
2018-05-26 10:12:32 -0300

    [29] Fiz teste de como inicializar a interface grafica.

--- gerenciador-curso-java/src/main/java/Main.java
+++ gerenciador-curso-java/src/main/java/Main.java
@@ -3,16 +3,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+        Parent root = FXMLLoader.load(getClass().getResource("/App/View/Professor/Cadastrar.fxml"));
+
         Scene scene = new Scene(root);
-        
+        stage.centerOnScreen();
+        stage.setHeight(Screen.getPrimary().getVisualBounds().getHeight());
+        stage.setWidth(Screen.getPrimary().getVisualBounds().getWidth());
+        stage.setTitle("Cadastro de professores");
+
         stage.setScene(scene);
         stage.show();
     }
@@ -23,5 +28,5 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }

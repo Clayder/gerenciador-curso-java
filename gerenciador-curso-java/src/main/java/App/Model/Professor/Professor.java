9b5937ace62c2646980d4330d62faa7dd585b83c
Clayder <peterclayder@gmail.com>
2018-05-31 19:21:02 -0300

    [30] Criei o novo construtor da classe professor.

--- gerenciador-curso-java/src/main/java/App/Model/Professor/Professor.java
+++ gerenciador-curso-java/src/main/java/App/Model/Professor/Professor.java
@@ -8,4 +8,16 @@ public class Professor extends ProfessorDAO implements IProfessor {
         super();
     }
 
+    /**
+     * 
+     * @param matricula
+     * @param nome
+     * @param endereco
+     * @param telefone
+     * @param valorHora
+     * @throws SQLException 
+     */
+    public Professor(String matricula, String nome, String endereco, String telefone, double valorHora) throws SQLException {
+        super(matricula, nome, endereco, telefone, valorHora);
+    }
 }

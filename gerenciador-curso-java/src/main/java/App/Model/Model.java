d84bb43004d8c945ac624160a5631e106e83f4cd
Clayder <peterclayder@gmail.com>
2018-06-03 11:51:11 -0300

    [30] Criei o método que verifica se um registro existe no banco.

--- gerenciador-curso-java/src/main/java/App/Model/Model.java
+++ gerenciador-curso-java/src/main/java/App/Model/Model.java
@@ -3,15 +3,18 @@ package App.Model;
 import App.Dao.Dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import org.apache.log4j.Logger;
 
-public abstract class Model extends Dao{
-    
-    public Model() throws SQLException{
+public abstract class Model extends Dao {
+
+    Logger log = Logger.getLogger(Model.class.getName());
+
+    public Model() throws SQLException {
         super();
     }
 
     /**
-     * 
+     *
      * @return @throws SQLException
      */
     public ResultSet findAll() throws SQLException {
@@ -44,29 +47,30 @@ public abstract class Model extends Dao{
         statement.setInt(1, id);
         statement.execute();
     }
-    
-      /**
-     * 
-     * @return String "INSERT INTO " + TABELA + " (matricula, nome, endereco, telefone, valor_hora) VALUES (?, ?, ?, ?, ?)"
+
+    /**
+     *
+     * @return String "INSERT INTO " + TABELA + " (matricula, nome, endereco,
+     * telefone, valor_hora) VALUES (?, ?, ?, ?, ?)"
      */
-    protected String getQueryInsert(){
+    protected String getQueryInsert() {
         String campos = "";
         String values = "";
-        
-        int tam  = this.colunasBD.size();
-        
-        for(String coluna : this.colunasBD){
+
+        int tam = this.colunasBD.size();
+
+        for (String coluna : this.colunasBD) {
             tam--;
             campos += coluna;
             values += "?";
-            if(tam > 0){
+            if (tam > 0) {
                 campos += ", ";
                 values += ", ";
             }
         }
-        
+
         String insert;
-        
+
         insert = "INSERT INTO " + TABELA;
         insert += " (";
         insert += campos;
@@ -74,53 +78,79 @@ public abstract class Model extends Dao{
         insert += " VALUES (";
         insert += values;
         insert += ")";
-        
+
         return insert;
     }
-    
+
     /**
      *
-     * @return String UPDATE tabela SET matricula = ?, nome = ?, endereco = ?, telefone = ?, valor_hora = ? WHERE id = ?
+     * @return String UPDATE tabela SET matricula = ?, nome = ?, endereco = ?,
+     * telefone = ?, valor_hora = ? WHERE id = ?
      */
-    protected String getQueryUpdate(){
+    protected String getQueryUpdate() {
         String campos = "";
-        
-        int tam  = this.colunasBD.size();
-        
-        for(String coluna : this.colunasBD){
+
+        int tam = this.colunasBD.size();
+
+        for (String coluna : this.colunasBD) {
             tam--;
             campos += coluna + " = ?";
-            if(tam > 0){
+            if (tam > 0) {
                 campos += ", ";
             }
         }
-        
+
         String insert = "";
-        insert += "UPDATE "+ TABELA +" SET ";
+        insert += "UPDATE " + TABELA + " SET ";
         insert += campos;
         insert += " WHERE id = ?";
-        
+
         return insert;
     }
-    
+
     /**
-     * 
-     * @throws SQLException 
+     *
+     * @param valor
+     * @param campo
+     * @return
+     * @throws SQLException
+     */
+    public boolean existe(String valor, String campo) throws SQLException {
+        String query = "SELECT count( " + campo + " ) as qty FROM " + TABELA
+                + " where " + campo + " = ?";
+
+        statement = connection.prepareStatement(query);
+        statement.setString(1, valor);
+        ResultSet res = statement.executeQuery();
+        int qty = 0;
+        while (res.next()) {
+            qty = res.getInt("qty");
+        }
+        if (qty == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     *
+     * @throws SQLException
      */
     public abstract void insert() throws SQLException;
-    
+
     /**
-     * 
-     * @throws SQLException 
+     *
+     * @throws SQLException
      */
     public abstract void update() throws SQLException;
-    
+
     /**
-     * 
+     *
      * @param id
-     * @throws SQLException 
+     * @throws SQLException
      */
     public abstract void setById(Integer id) throws SQLException;
-    
+
     protected abstract void setCampos();
 }
